e29417fdc632e42420331bda11a3b1f0fcf8a0a5
Benoit METHENS <baby3195@homail.com>
2026-09-12 11:38:38 +0200

    Renomme le paramètre "motif" en "Motif" dans EligibiliteResult - (convention de nommage C#)

--- FDL.Loc/App/EligibiliteResult.cs
+++ FDL.Loc/App/EligibiliteResult.cs
@@ -1,6 +1,6 @@
 ﻿namespace FDL.Loc.App
 {
-    public record EligibiliteResult(bool EstEligible, string motif)
+    public record EligibiliteResult(bool EstEligible, string Motif)
     {
         public static EligibiliteResult Eligible(string motif) => new (true,motif);
         public static EligibiliteResult NonEligible(string motif) => new (false,motif);
