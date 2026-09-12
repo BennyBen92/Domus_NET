db7470cefb189949c64a4d8e153e25f4b9e6bf40
Benoit METHENS <baby3195@homail.com>
2026-09-12 13:46:51 +0200

    Interface Registre

---
+++ FDL.Loc/App/IRegistreRepository.cs
@@ -0,0 +1,11 @@
+﻿using FDL.Core.App;
+using FDL.Loc.Domain;
+
+namespace FDL.Loc.App
+{
+    internal interface IRegistreRepository : IRepositoryReadBase<Registre>, IRepositoryWriteBase<Registre>
+    {
+        Registre GetByContrat(int idContrat);
+        
+    }
+}
