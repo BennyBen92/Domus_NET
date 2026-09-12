422f403790e02b95f252c5d60f3c5893a1a73c37
Benoit METHENS <baby3195@homail.com>
2026-09-12 13:47:05 +0200

    Interface Workflow

---
+++ FDL.WF/App/IWorkflowRepository.cs
@@ -0,0 +1,12 @@
+﻿using FDL.Core.App;
+using FDL.WF.Domain;
+
+namespace FDL.WF.App
+{
+    internal interface IWorkflowRepository : IRepositoryReadBase<Workflow>, IRepositoryWriteBase<Workflow>
+    {
+        Workflow GetByUserAssigned(int idUser);
+        Workflow GetByGroupAssigned(int idGroup);
+
+    }
+}
