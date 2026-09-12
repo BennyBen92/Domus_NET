36141afe09f188831ba64e1a79b2eeccf27df228
Benoit METHENS <baby3195@homail.com>
2026-09-12 16:12:00 +0200

    Tests ajout

---
+++ FDL.Core.Tests/ReferenceDossierTests.cs
@@ -0,0 +1,26 @@
+﻿using FDL.Core.Domain;
+
+namespace FDL.Core.Tests
+{
+    public class ReferenceDossierTests
+    {
+        [Fact]
+        public void Creation_AvecSequence0_EstRefusee()
+        {
+
+        }
+        [Fact]
+        public void DepuisExistant_AvecSequence0_EstUneDemandeHeritee()
+        {
+
+        }
+        [Theory]
+        [InlineData(1_500_123, 61)]
+        [InlineData(9_999_999, 1)]
+        public void ToString_PuisParse_RedonneLaMemeReference(int dossier, int sequence)
+        {
+            var origine = new ReferenceDossier(dossier, sequence);
+            Assert.Equal(origine, ReferenceDossier.Parse(origine.ToString()));
+        }
+    }
+}
---
+++ FDL.Loc.Tests/ReferenceDossierLocTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FDL.Loc.Tests
+{
+    public class ReferenceDossierLocTests
+    {
+        [Fact]
+        public void Registre_AvecReferenceDeDemande_EstAccepte()
+        {
+
+        }
+        [Fact]
+        public void Registre_AvecReferenceDeContrat_LeveUneException()
+        {
+
+        }
+    }
+}
