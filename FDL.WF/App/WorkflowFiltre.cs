d891f00788a7d068692720956f1f3e96231e4303
Benoit METHENS <baby3195@homail.com>
2026-09-20 20:07:22 +0200

    WorkflowFiltre: params avec valeurs par défaut

--- FDL.WF/App/WorkflowFiltre.cs
+++ FDL.WF/App/WorkflowFiltre.cs
@@ -1,4 +1,4 @@
 ﻿namespace FDL.WF.App
 {
-    public sealed record WorkflowFiltre(int? IdUser, int? IdGroup, bool? EstTermine);
+    public sealed record WorkflowFiltre(int? IdUser = null, int? IdGroup = null, bool? EstTermine = null);
 }
