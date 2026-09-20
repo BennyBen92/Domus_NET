16d25f7dbd48fbdd5738d0f24ff915803796e991
Benoit METHENS <baby3195@homail.com>
2026-09-20 19:12:30 +0200

    Correction IWorkflowRepository pour gérer une liste.

--- FDL.WF/App/IWorkflowRepository.cs
+++ FDL.WF/App/IWorkflowRepository.cs
@@ -5,8 +5,11 @@ namespace FDL.WF.App
 {
     public interface IWorkflowRepository : IReadRepository<Workflow>, IWriteRepository<Workflow>
     {
-        Workflow GetByUserAssigned(int idUser);
-        Workflow GetByGroupAssigned(int idGroup);
+        IReadOnlyList<Workflow> GetByUserAssigned(int idUser);
+        IReadOnlyList<Workflow> GetByGroupAssigned(int idGroup);
 
+        // Pour récupérer par exemple les wf "terminés" assignés à un utilisateur,
+        // Comment faire ? On utilise GetByUserAssigned et on filtre sur IdUserTermination != 0 ?
+        // Ou on ajoute une méthode GetByUserAssignedTerminated(int idUser) ?
     }
 }
