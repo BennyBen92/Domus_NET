502b8bd933394dbab7d2eeaf33237a7c4d958f04
Benoit METHENS <baby3195@homail.com>
2026-09-22 16:50:37 +0200

    Workflow Tests projet ajouté

---
+++ FDL.WF.Tests/WorkflowTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Time.Testing;
+
+namespace FDL.WF.Tests
+{
+    public class WorkflowTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            FakeTimeProvider fakeTime = new();
+            throw new NotImplementedException();
+        }
+    }
+}
