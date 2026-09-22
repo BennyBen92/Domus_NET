5f598e266de9eb51a8e2212a649bf16a13363453
Benoit METHENS <baby3195@homail.com>
2026-09-22 18:44:28 +0200

    Déplacement de SessionUtilisateur

---
+++ FDL.Infra/SessionUtilisateur.cs
@@ -0,0 +1,34 @@
+﻿using FDL.Core.App;
+using FDL.Core.Domain;
+
+namespace FDL.Infra
+{
+    public sealed class SessionUtilisateur : IUtilisateurCourant
+    {
+        private Utilisateur? _utilisateur;
+        private IReadOnlyList<int> _idGroupes = [];
+        // ------------------------------------------------------------
+
+        public Utilisateur UtilisateurConnecte => _utilisateur ?? throw new InvalidOperationException("L'utilisateur n'est pas connecté.");
+        public int Id => UtilisateurConnecte.Id;
+        public IReadOnlyList<int> IdGroupes
+        {
+            get { _ = UtilisateurConnecte; return _idGroupes; }
+        }
+        // ------------------------------------------------------------
+
+
+        public void Connecter(Utilisateur utilisateur)
+        {
+            ArgumentNullException.ThrowIfNull(utilisateur, nameof(utilisateur));
+            _utilisateur = utilisateur;
+            _idGroupes = [.. utilisateur.Groupes.Select(g => g.Id)];
+        }
+
+        public void Deconnecter()
+        {
+            _utilisateur = null;
+            _idGroupes = [];
+        }
+    }
+}
